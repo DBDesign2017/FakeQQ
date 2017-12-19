8a4712d54d3d1d5b72efa3e2cf340c5391bdb9f4
Aleller <1640799791@qq.com>
2017-12-19 21:16:34 +0800

    增加了普通用户登录时往数据库里面写记录的功能。

--- FakeQQ_Server/ServerOperation.cs
+++ FakeQQ_Server/ServerOperation.cs
@@ -148,6 +148,7 @@ namespace FakeQQ_Server
                         }
                     }
                 }
+                ToUpdateOnlineUserList(null, null);
             }
         }
 
@@ -326,6 +327,32 @@ namespace FakeQQ_Server
                             }
                             //发布OneUserLogin事件
                             ToUpdateOnlineUserList(null, null);
+                            //在数据库的Stat表中插入一条记录
+                            try
+                            {
+                                SqlConnection insertConnect = new SqlConnection("Data Source=" + DataSourceName + ";Initial Catalog=JinNangIM_DB;Integrated Security=True");
+                                SqlCommand insertCmd = new SqlCommand("insert into dbo.Stat values('" + DateTime.Now + "', '" + line.UserID + "', '" + "Login" + "')", insertConnect);
+                                if (insertConnect.State == ConnectionState.Closed)
+                                {
+                                    try
+                                    {
+                                        insertConnect.Open();
+                                        insertCmd.ExecuteNonQuery();
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e.ToString());
+                                    }
+                                    finally
+                                    {
+                                        insertConnect.Close();
+                                    }
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.ToString());
+                            }
                             break;
                         }
                     case 2://客户端登录失败
